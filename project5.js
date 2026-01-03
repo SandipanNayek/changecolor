2bd9165231a230a501e28f9b8847b91f23dd99c1
SandipanNayek <sandipannayek09@gmail.com>
2026-01-04 00:35:27 +0530

    Add files via upload

---
+++ project5.js
@@ -0,0 +1,30 @@
+const randomColor=function(){
+    const hex='0123456789ABCDEF'
+    let color='#'
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+  let intervalId
+
+  const changeBgcolor=function(){
+    document.body.style.backgroundColor=randomColor();
+   }
+
+const startChangingColor=function(){
+    if(!intervalId){
+    intervalId=setInterval(changeBgcolor,1000);
+    }
+}
+
+   const stopChangingColor=function(){
+    clearInterval(intervalId)
+    intervalId=null;
+}
+ 
+
+
+document.querySelector("#start").addEventListener('click',startChangingColor)
+
+document.querySelector("#stop").addEventListener('click',stopChangingColor)
